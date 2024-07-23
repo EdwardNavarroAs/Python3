cc8fa4b7034a8ccbfa5cc9f64d3889e8ca699a3c
EdwardNavarroAs <edwardgrialunicauca@gmail.com>
2024-07-23 09:36:36 -0500

    agregando las reglas para nombrar variables y como se asignan multiples valores de variables en python

--- 1_Variables/1_variables.py
+++ 1_Variables/1_variables.py
@@ -31,3 +31,40 @@ A = "Sally"
 print(f"variable a: {a}, es diferente de variable A: {A}")
 print('')
 
+# Reglas para nombrar variables en Python:
+"""El nombre de una variable debe comenzar con una letra o un carácter de subrayado.
+El nombre de una variable no puede comenzar con un número.
+El nombre de una variable solo puede contener caracteres alfanuméricos y guiones bajos (Az, 0-9 y _).
+Los nombres de las variables distinguen entre mayúsculas y minúsculas (edad, Edad y EDAD son variables diferentes)
+Un nombre de variable no puede ser ninguna de las palabras clave de Python .
+
+ej variables legales:               ej variables ilegales
+
+myvar = "John"                      2myvar = "John"
+my_var = "John"                     my-var = "John"
+_my_var = "John"                    my var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+"""
+print("nombrando variables en python")
+# metodo Carmel: Cada palabra, excepto la primera, comienza con mayúscula (mas utilizada)
+myVariableName = "myVariableName"
+# metodo Pascal: Cada palabra comienza con una letra mayúscula
+MyVariableName = "MyVariableName"
+# metodo Snake: Cada palabra está separada por un guion bajo
+my_variable_name = "my_variable_name"
+print(f"metodo carmel: {myVariableName}, metodo pascal: {MyVariableName}, metodo snake: {my_variable_name}")
+print('')
+
+# Python permite asignar valores a múltiples variables en una sola línea
+print("asignando multiples valores de variables")
+x, y, z = "Orange", "Banana", "Cherry"
+print(f"variable 1: {x}, variable 2: {y}, variable 3: {z}")
+print('')
+
+# python permite asignar el mismo valor a múltiples variables en una línea:
+print("asignando el mismo valor a multiples variables")
+x = y = z = 10
+print(f"variable 1: {x}, variable 2: {y}, variable 3: {z}")
+print('')
