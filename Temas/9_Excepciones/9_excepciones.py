b753011b54905042e02c23e0845de7cd0dd6dd8f
EdwardNavarroAs <edwardgrialunicauca@gmail.com>
2024-09-25 11:53:49 -0500

    Iniciando con el tema sobre excepciones en python

---
+++ Temas/9_Excepciones/9_excepciones.py
@@ -0,0 +1,107 @@
+# Tema 9: Excepciones en Python
+
+# Cuando ocurre un error, o una excepción como la llamamos, Python normalmente se detiene y genera un mensaje de error.
+# Las excepciones en Python se manejan mediante diferentes bloques:
+
+"""
+El bloque try permite probar un bloque de código para detectar errores.
+
+El bloque except permite gestionar el error que se haya producido.
+
+El bloque else permite ejecutar código cuando no hay ningún error.
+
+El bloque finally permite ejecutar código, independientemente de si se produjo o no una excepción.
+"""
+
+# Ejemplo 1 de manejo de excepciones: una variable no definida.
+# Dado que el bloque try genera un error, se ejecuta el bloque except.
+# Sin el bloque try, el programa se detiene y genera un error:
+try:
+    print("try permite probar un bloque de código para detectar errores.")
+    print("Ejemplo 1: Intentando ejecutar un bloque de código donde una variable no está definida.")
+    print(x)  # Esto causará una excepción ya que x no está definida
+except:
+    print("Error: Ha ocurrido una excepción general.")
+    print('')
+
+# Es posible definir tantos bloques de excepción como sea necesario, por ejemplo, 
+# si es necesario ejecutar un bloque de código especial para un tipo específico de error:
+try:
+    print("Es posible definir tantos bloques de excepción como sea necesario.")
+    print("Ejemplo 2: Manejando una excepción específica cuando una variable no está definida.")
+    print(x)  # Esto causará una excepción
+except NameError:
+    print("Error: la variable no ha sido definida.")
+    print('')
+except:
+    print("Error: Ha ocurrido una excepción general.")
+    print('')
+
+# En Python es posible utilizar la palabra clave else para definir un bloque de código que se ejecuta 
+# solo si no se produjeron errores:
+try:
+    print("La palabra clave else se utiliza para definir un bloque de código que se ejecuta si no se produce ningún error.")
+    print("Ejemplo 3: Uso de la palabra else en un bloque de código que no genera ninguna excepción.")
+except:
+    print("Error: Ha ocurrido una excepción general.")
+    print('')
+else:
+    print("No se ha generado ninguna excepción.")
+    print("Se ejecuta el bloque else.")
+    print('')
+
+# El bloque finally, si se especifica, se ejecuta independientemente de si el bloque try genera un error o no.
+try:
+    print("El bloque finally se ejecuta independientemente de si el bloque try genera un error o no.")
+    print("Ejemplo 4: Ejecutando un bloque finally independientemente de si se genera una excepción en el código.")
+    print(x)  # Esto causará una excepción
+except:
+    print("Error: Ha ocurrido una excepción general.")
+finally:
+    print("El bloque try-except ha finalizado.")
+    print('')
+
+# El bloque finally se utiliza comúnmente para cerrar objetos y limpiar recursos: 
+try:
+    print("El bloque finally se utiliza comúnmente para cerrar objetos y limpiar recursos.")
+    print("Ejemplo 5: Utilizando el bloque finally para cerrar objetos o recursos abiertos.")
+    f = open("demofile.txt")
+    try:
+        f.write("Lorem Ipsum")
+    except:
+        print("Error: Ha ocurrido una excepción al escribir en el archivo de texto.")
+    finally:
+        f.close()
+except:
+    print("Error: Ha ocurrido una excepción al abrir el archivo de texto.")
+finally:
+    print("El programa puede continuar sin dejar abierto el archivo.")
+    print('')
+
+# En Python, es posible generar una excepción si se cumplen ciertas condiciones.
+# Para generar una excepción, se debe utilizar la palabra clave raise.
+try:
+    print("Es posible generar una excepción si se cumplen ciertas condiciones utilizando la palabra raise.")
+    print("Ejemplo 6: Utilizando la palabra raise para generar una excepción general.")
+    x = -1
+    if x < 0:
+        raise Exception("Error: El código no permite números negativos.")
+except Exception as e:
+    print("Error: Ha ocurrido una excepción general: ", str(e))
+    print('')
+
+# Es posible definir qué tipo de error generar y el texto que se mostrará al usuario.
+try:
+    print("La palabra raise permite definir el tipo de error generado y el texto presentado al usuario.")
+    print("Ejemplo 7: Utilizando la palabra raise para generar una excepción específica.")
+    
+    x = "hello"
+    
+    if not type(x) is int:
+        raise TypeError("Error: El código solo permite números enteros.")
+except TypeError as e:
+    print("Error: Ha ocurrido un error de tipado: ", str(e))
+    print('')
+except Exception as e:
+    print("Error: Ha ocurrido una excepción general: ", str(e))
+    print('')
